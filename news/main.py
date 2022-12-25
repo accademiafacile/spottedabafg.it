9f70fc9bf932eda49e9e4a9e4d33fa98515589fc
accademiafacile <accademiafacile@gmail.com>
2022-12-26 00:29:05 +0100

    Update main.py

--- news/main.py
+++ news/main.py
@@ -9,20 +9,6 @@ import watchdog
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-class CommitPushHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        # Create a Git repository object
-        repo = git.Repo("./")
-
-        # Stage the news.html file for commit
-        repo.index.add(["news.html"])
-
-        # Create a commit with a message
-        repo.index.commit("Updating news.html")
-
-        # Push the commit to the repository's origin branch
-        origin = repo.remote(name="origin")
-        origin.push()
 
 def shorten_url(long_url):
   # Remove "https://" from the start of the URL
@@ -81,11 +67,27 @@ while True:
     # Save the modified HTML page to a file
     with open('../news.html', 'w') as f:
         f.write(template)
+     
+    
+    class CommitPushHandler(FileSystemEventHandler):
+        def on_modified(self, event):
+            # Create a Git repository object
+            repo = git.Repo("./")
+
+            # Stage the news.html file for commit
+            repo.index.add(["news.html"])
+
+            # Create a commit with a message
+            repo.index.commit("Updating news.html")
+
+            # Push the commit to the repository's origin branch
+            origin = repo.remote(name="origin")
+            origin.push() 
         
     if __name__ == "__main__":
         event_handler = CommitPushHandler()
         observer = Observer()
-        observer.schedule(event_handler, path="./", recursive=False)
+        observer.schedule(event_handler, path="../", recursive=False)
         observer.start()
 
     try:
